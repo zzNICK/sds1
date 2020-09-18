854e93aa3822d2f71e751e4a5d3676ab835aa109
Nicolas Correia <nicononoya@gmail.com>
2020-09-17 21:48:35 -0300

    Adicionando componente header

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Hello semana dev superior!</h1>
+    <div>
+      <Header />
+    </div>
   );
 }
 
